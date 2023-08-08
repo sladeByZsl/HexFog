4333036021209489b7aa09ff383739a15528a193
zhousilai <zhousilai@elex-tech.com>
2023-08-08 19:19:28 +0800

    调整迷雾代码

--- Assets/HexFog/Scripts/TestHexFog.cs
+++ Assets/HexFog/Scripts/TestHexFog.cs
@@ -5,16 +5,51 @@ using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 
 //[ExecuteAlways]
 public class TestHexFog : MonoBehaviour
 {
-    //public Texture2D _hexMask;
-    public Camera m_cam;
-    public Mesh m_maskMesh;
-    public Material m_mat;
-    public RenderTexture m_fogRT;
-    public Renderer Plan;
+    [Header("六边形遮罩的mesh")]
+    public Mesh maskMesh;
+    
+    [Header("迷雾的材质球")] 
+    public Material fogMaterial;
+    
+    [Header("迷雾RT")] 
+    public RenderTexture fogRT;
+
+    [Header("迷雾RT的Size")]
+    public Vector2Int fogRTSize = new Vector2Int(256, 256);
+    
+    [Header("0层颜色")]
+    public static readonly Color FogColor0=Color.black;
+    [Header("1层颜色")]
+    public static readonly Color FogColor1=Color.green;
+    [Header("2层颜色")]
+    public static readonly Color FogColor2=Color.blue;
+    
+    [Header("地表")]
+    public Renderer planeRender;
+    //地表shader参数
+    private static readonly int baseMap = Shader.PropertyToID("_BaseMap");
+
+    #region 视野矩阵
+    public Vector3 viewTransform = new Vector3(0, 20, 0);
+    private static readonly Quaternion viewQuaternion = Quaternion.Euler(90, 0, 0);
+    private static readonly Vector3 viewScale = new Vector3(1, 1, -1);
+    #endregion
+
+    #region 正交投影
+    public float viewLeft = -50.0f;
+    public float viewRight = 50.0f;
+    public float viewBottom = -50.0f;
+    public float viewTop = 50.0f;
+    public float viewNear = 0f;
+    public float viewFar = 100.0f;
+    #endregion
+
+    private const string HexFogCbufferName = "DrawFog";
 
     public Vector2 m_visiblaRange;
     public float m_hexSize;
@@ -40,77 +75,64 @@ public class TestHexFog : MonoBehaviour
         new Vector3(25.98f, 0.00f, 45.00f),
         new Vector3(43.30f, 0.00f, 45.00f),
     };
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         if (m_cbuffer == null)
         {
             m_cbuffer = new CommandBuffer();
         }
-
-        if (m_fogRT == null)
+        if (fogRT == null)
         {
-            m_fogRT = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGB32);
+            fogRT = new RenderTexture(fogRTSize.x, fogRTSize.y, 0, RenderTextureFormat.ARGB32);
         }
-
-        Plan.sharedMaterial.SetTexture("_BaseMap", m_fogRT);
-        m_cbuffer.name = "DrawFog";
-        m_viewMatrix = Matrix4x4.TRS(new Vector3(0, 20, 0), Quaternion.Euler(90, 0, 0), new Vector3(1, 1, -1));
+        planeRender.sharedMaterial.SetTexture(baseMap, fogRT);
+        m_cbuffer.name = HexFogCbufferName;
+        m_viewMatrix = Matrix4x4.TRS(viewTransform, viewQuaternion, viewScale);
     }
 
-    void Update()
+    private Matrix4x4[] Convert2Matrix(Vector3[] positions)
     {
-        // DrawHexImmediately();
-    }
-
-    public Matrix4x4[] ConverToMatrxs(Vector3[] Positions)
-    {
-        if (Positions.Length > 0)
+        if (positions.Length > 0)
         {
-            Matrix4x4[] mx = new Matrix4x4[Positions.Length];
-
-            for (int i = 0; i < Positions.Length; i++)
+            Matrix4x4[] mx = new Matrix4x4[positions.Length];
+            for (int i = 0; i < positions.Length; i++)
             {
-                mx[i] = Matrix4x4.TRS(Positions[i], Quaternion.identity, Vector3.one*m_hexSize );
+                mx[i] = Matrix4x4.TRS(positions[i], Quaternion.identity, Vector3.one*m_hexSize );
             }
-
             return mx;
         }
-
         return null;
     }
 
-    public void DrawHexImmediately(Vector3[] positions)
+    private void DrawHexImmediately(Vector3[] positions)
     {
-        if (m_fogRT == null)
+        if (fogRT == null)
         {
             Debug.LogError("fog rt is null");
             return;
         }
 
-        var matrixes = ConverToMatrxs(positions);
-        DrawHexMesh(matrixes,true);
+        var matrixArray = Convert2Matrix(positions);
+        DrawHexMesh(matrixArray,true);
     }
 
-
+    #region 渐变效果
     public bool StopdrawHexAsync;
-
     public void DrawHexAync2(Vector3[] positions, float[] maskdirection, bool clear = false)
     {
         StartCoroutine(DrawHexAsync(positions, maskdirection));
     }
 
-
     IEnumerator DrawHexAsync(Vector3[] positions, float[] maskdirection)
     {
-        if (m_fogRT == null)
+        if (fogRT == null)
         {
             Debug.LogError("fog rt is null");
             yield break;
         }
 
-        var matrixes = ConverToMatrxs(positions);
+        var matrixes = Convert2Matrix(positions);
         var _Dissolve = 1f;
         var stop = false;
         var buffer = new float[matrixes.Length];
@@ -147,26 +169,29 @@ public class TestHexFog : MonoBehaviour
         }
     }
 
+    
 
+    #endregion
+   
     void DrawHexMesh(Matrix4x4[] matries, bool clear = false, MaterialPropertyBlock properties = null)
     {
-        Graphics.SetRenderTarget(m_fogRT);
+        Graphics.SetRenderTarget(fogRT);
         if (clear)
         {
-            m_cbuffer.ClearRenderTarget(true, true, Color.blue);
+            m_cbuffer.ClearRenderTarget(true, true,FogColor2);
         }
 
-        m_cbuffer.SetViewProjectionMatrices(m_viewMatrix.inverse, GL.GetGPUProjectionMatrix(Matrix4x4.Ortho(-50, 50, -50, 50, 0, 100), false));
+        m_cbuffer.SetViewProjectionMatrices(m_viewMatrix.inverse, GL.GetGPUProjectionMatrix(Matrix4x4.Ortho(viewLeft, viewRight, viewBottom, viewTop, viewNear, viewFar), false));
         if (properties != null)
         {
-            m_cbuffer.DrawMeshInstanced(m_maskMesh, 0, m_mat, 0, matries, matries.Length, properties);
+            m_cbuffer.DrawMeshInstanced(maskMesh, 0, fogMaterial, 0, matries, matries.Length, properties);
         }
         else
         {
             //matries = new Matrix4x4[1];
             //matries[0] = Matrix4x4.TRS(  new Vector3(17.32f, 0.00f, 30.00f),quaternion.identity, Vector3.one);
             //m_cbuffer.DrawMeshInstancedProcedural(m_maskMesh,0,m_mat,0,matries.Length);
-            m_cbuffer.DrawMeshInstanced(m_maskMesh, 0, m_mat, 0, matries, matries.Length);
+            m_cbuffer.DrawMeshInstanced(maskMesh, 0, fogMaterial, 0, matries, matries.Length);
         }
 
         //m_cbuffer.Clear();
@@ -177,7 +202,7 @@ public class TestHexFog : MonoBehaviour
     {
         if (GUI.Button(new Rect(10, 10, 120, 80), "Blit"))
         {
-            Debug.LogError(m_cam.worldToCameraMatrix);
+            //Debug.LogError(m_cam.worldToCameraMatrix);
             Debug.LogError(m_viewMatrix.inverse);
             DrawHexImmediately(hexPos);
         }
